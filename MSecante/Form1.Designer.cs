bb342cfa7868d4bcd424b9d22b392337cd86a11c
Josmar Perez <craftero350@gmail.com>
2025-11-02 23:23:44 -0600

    Agregar archivos de proyecto.

---
+++ MSecante/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MSecante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFuncion = new System.Windows.Forms.TextBox();
+            this.dataIteracion = new System.Windows.Forms.DataGridView();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblFuncion = new System.Windows.Forms.Label();
+            this.lblRango = new System.Windows.Forms.Label();
+            this.txtX0 = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtError = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataIteracion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFuncion
+            // 
+            this.txtFuncion.Location = new System.Drawing.Point(115, 41);
+            this.txtFuncion.Name = "txtFuncion";
+            this.txtFuncion.Size = new System.Drawing.Size(161, 22);
+            this.txtFuncion.TabIndex = 0;
+            // 
+            // dataIteracion
+            // 
+            this.dataIteracion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataIteracion.Location = new System.Drawing.Point(50, 199);
+            this.dataIteracion.Name = "dataIteracion";
+            this.dataIteracion.RowHeadersWidth = 51;
+            this.dataIteracion.RowTemplate.Height = 24;
+            this.dataIteracion.Size = new System.Drawing.Size(684, 219);
+            this.dataIteracion.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(360, 41);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(360, 103);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblFuncion
+            // 
+            this.lblFuncion.AutoSize = true;
+            this.lblFuncion.Location = new System.Drawing.Point(55, 44);
+            this.lblFuncion.Name = "lblFuncion";
+            this.lblFuncion.Size = new System.Drawing.Size(54, 16);
+            this.lblFuncion.TabIndex = 5;
+            this.lblFuncion.Text = "Función";
+            // 
+            // lblRango
+            // 
+            this.lblRango.AutoSize = true;
+            this.lblRango.Location = new System.Drawing.Point(55, 75);
+            this.lblRango.Name = "lblRango";
+            this.lblRango.Size = new System.Drawing.Size(90, 16);
+            this.lblRango.TabIndex = 6;
+            this.lblRango.Text = "[            ,            ]";
+            // 
+            // txtX0
+            // 
+            this.txtX0.Location = new System.Drawing.Point(70, 72);
+            this.txtX0.Name = "txtX0";
+            this.txtX0.Size = new System.Drawing.Size(27, 22);
+            this.txtX0.TabIndex = 7;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Location = new System.Drawing.Point(127, 72);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(27, 22);
+            this.txtX1.TabIndex = 8;
+            // 
+            // txtError
+            // 
+            this.txtError.Location = new System.Drawing.Point(115, 100);
+            this.txtError.Name = "txtError";
+            this.txtError.Size = new System.Drawing.Size(161, 22);
+            this.txtError.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Error:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.txtX0);
+            this.Controls.Add(this.lblRango);
+            this.Controls.Add(this.lblFuncion);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.dataIteracion);
+            this.Controls.Add(this.txtError);
+            this.Controls.Add(this.txtFuncion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataIteracion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFuncion;
+        private System.Windows.Forms.DataGridView dataIteracion;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblFuncion;
+        private System.Windows.Forms.Label lblRango;
+        private System.Windows.Forms.TextBox txtX0;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtError;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MSecante/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSecante
+{
+    public partial class Form1 : Form
+    {
+        private float x0, x1, errorDeseado;
+        private int iteraciones;
+        private float raiz, errorFinal;
+        private List<IteracionSecante> tablaIteraciones;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private class IteracionSecante
+        {
+            public int Iteracion { get; set; }
+            public float X0 { get; set; }
+            public float XI { get; set; }
+            public float Fx0 { get; set; }
+            public float FxI { get; set; }
+            public float X2 { get; set; }
+            public float Error { get; set; }
+        }
+
+
+        private void CalcularSecante()
+        {
+            tablaIteraciones = new List<IteracionSecante>();
+
+            float xi0 = x0;
+            float xi1 = x1;
+            float errorActual = 1f;
+            int iter = 0;
+
+            float fx0 = EvaluarFuncion(xi0);
+            float fx1 = EvaluarFuncion(xi1);
+            float x2 = CalcularX2(xi0, xi1, fx0, fx1);
+
+            tablaIteraciones.Add(new IteracionSecante
+            {
+                Iteracion = 1,
+                X0 = xi0,
+                XI = xi1,
+                Fx0 = fx0,
+                FxI = fx1,
+                X2 = x2,
+                Error = 1f
+            });
+
+            iter++;
+            errorActual = Math.Abs((x2 - xi1) / x2);
+            xi0 = xi1;
+            xi1 = x2;
+
+            while (errorActual > errorDeseado && iter < 100)
+            {
+                fx0 = EvaluarFuncion(xi0);
+                fx1 = EvaluarFuncion(xi1);
+                x2 = CalcularX2(xi0, xi1, fx0, fx1);
+
+                if (float.IsNaN(x2) || float.IsInfinity(x2))
+                    throw new Exception("El método divergió o se produjo una división por cero.");
+
+                errorActual = Math.Abs((x2 - xi1) / x2);
+
+                tablaIteraciones.Add(new IteracionSecante
+                {
+                    Iteracion = iter + 1,
+                    X0 = xi0,
+                    XI = xi1,
+                    Fx0 = fx0,
+                    FxI = fx1,
+                    X2 = x2,
+                    Error = errorActual
+                });
+
+                xi0 = xi1;
+                xi1 = x2;
+                iter++;
+            }
+
+            if (errorActual > errorDeseado)
+                throw new Exception("No se encontró raíz en 100 iteraciones.");
+
+            iteraciones = iter;
+            raiz = x2;
+            errorFinal = errorActual;
+        }
+
+        private float EvaluarFuncion(float x)
+        {
+            string funcion = txtFuncion.Text;
+            if (string.IsNullOrWhiteSpace(funcion))
+                throw new Exception("Debe ingresar una función válida.");
+
+            // Reemplazar '^' por Math.Pow simple
+            string expr = funcion.Replace("^", "**").Replace("x", x.ToString(System.Globalization.CultureInfo.InvariantCulture));
+
+            while (expr.Contains("**"))
+            {
+                int idx = expr.IndexOf("**");
+                int start = idx - 1;
+                int end = idx + 2;
+
+                string baseNum = expr[start].ToString();
+                string exponente = expr[end].ToString();
+                string powRepl = Math.Pow(double.Parse(baseNum), double.Parse(exponente)).ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+                expr = expr.Substring(0, start) + powRepl + expr.Substring(end + 1);
+            }
+
+            var dt = new DataTable();
+            var result = dt.Compute(expr, "");
+            return Convert.ToSingle(result);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtX0.Clear();
+            txtX1.Clear();
+            txtError.Clear();
+            txtFuncion.Clear();
+            dataIteracion.DataSource = null;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Confirmación antes de ejecutar el cálculo
+            if (MessageBox.Show("Revise por última vez. ¿La función es correcta?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return; // Si el usuario responde "No", salimos
+            }
+
+            try
+            {
+                // Validar campos
+                if (string.IsNullOrWhiteSpace(txtX0.Text) || string.IsNullOrWhiteSpace(txtX1.Text) ||
+                    string.IsNullOrWhiteSpace(txtError.Text) || string.IsNullOrWhiteSpace(txtFuncion.Text))
+                {
+                    MessageBox.Show("Por favor complete todos los campos.", "Campos vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!float.TryParse(txtX0.Text, out x0) ||
+                    !float.TryParse(txtX1.Text, out x1) ||
+                    !float.TryParse(txtError.Text, out errorDeseado))
+                {
+                    MessageBox.Show("Ingrese valores numéricos válidos.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (errorDeseado <= 0)
+                {
+                    MessageBox.Show("El error debe ser positivo.", "Error de entrada", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Ejecutar cálculo
+                CalcularSecante();
+                MostrarResultados();  // Llamada a la función de mostrar resultados
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error de cálculo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MostrarResultados()
+        {
+            // Mostrar los resultados de las iteraciones en el DataGridView
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Iteración");
+            dt.Columns.Add("x0");
+            dt.Columns.Add("x1");
+            dt.Columns.Add("f(x0)");
+            dt.Columns.Add("f(x1)");
+            dt.Columns.Add("x2");
+            dt.Columns.Add("ε");
+
+            foreach (var iter in tablaIteraciones)
+            {
+                dt.Rows.Add(
+                    iter.Iteracion,
+                    iter.X0.ToString("F6"),
+                    iter.XI.ToString("F6"),
+                    iter.Fx0.ToString("F6"),
+                    iter.FxI.ToString("F6"),
+                    iter.X2.ToString("F6"),
+                    iter.Error.ToString("F6")
+                );
+            }
+
+            // Asignar el DataTable al DataGridView
+            dataIteracion.DataSource = dt;
+
+            // Mostrar el resumen en un MessageBox
+            string resumen = $"Método de la Secante\n\n" +
+                             $"Función: {txtFuncion.Text}\n" +
+                             $"x0 = {x0},  x1 = {x1}\n" +
+                             $"Error deseado = {errorDeseado}\n\n" +
+                             $"Iteraciones realizadas: {iteraciones}\n" +
+                             $"Raíz aproximada: {raiz:F6}\n" +
+                             $"Error final: {errorFinal:F6}";
+
+            MessageBox.Show(resumen, "Resumen del cálculo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private float CalcularX2(float x0, float x1, float fx0, float fx1)
+        {
+            if (Math.Abs(fx1 - fx0) < 1e-6)
+                throw new Exception("División por cero: f(x1) ≈ f(x0).");
+
+            return x1 - (fx1 * (x1 - x0)) / (fx1 - fx0);
+        }
+    }
+}
